5d8ec96f98ec0da5973943e07fabe934304a55a6
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-02-05 22:15:41 +0900

    fix arg definition & specify value_parser

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,12 @@ use std::error::Error;
 use clap::{Parser};
 
 #[derive(Parser, Debug)]
-#[command(name = "headr", version = "0.1.0", author = "Radish-Miyazaki <y.hidaka.kobe@gmail.com>", about = "Rust head")]
+#[command(
+    name = "headr",
+    version = "0.1.0",
+    author = "Radish-Miyazaki <y.hidaka.kobe@gmail.com>",
+    about = "Rust head",
+)]
 pub struct Args {
     #[arg(
         value_name = "FILE",
@@ -10,24 +15,41 @@ pub struct Args {
         default_value = "-"
     )]
     files: Vec<String>,
+
     #[arg(
         value_name = "LINES",
         help = "Number of lines",
         short = 'n',
         long = "lines",
+        value_parser = parse_lines_positive_int,
         default_value = "10",
-        value_parser = clap::value_parser!(u64).range(1..),
     )]
-    lines: u64,
+    lines: usize,
+
     #[arg(
         value_name = "BYTES",
         help = "Number of bytes",
         short = 'c',
         long = "bytes",
+        value_parser = parse_bytes_positive_int,
         conflicts_with = "lines",
-        value_parser = clap::value_parser!(u64).range(1..),
     )]
-    bytes: Option<u64>,
+    bytes: Option<usize>,
+}
+
+fn parse_lines_positive_int(s: &str) -> Result<usize, String> {
+    parse_positive_int(s, "line")
+}
+
+fn parse_bytes_positive_int(s: &str) -> Result<usize, String> {
+    parse_positive_int(s, "byte")
+}
+
+fn parse_positive_int(s: &str, typ: &str) -> Result<usize, String> {
+    match s.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(format!("illegal {} count -- {}", typ, s))
+    }
 }
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
